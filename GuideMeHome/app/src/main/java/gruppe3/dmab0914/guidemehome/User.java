ea014f3deaacbaa00d0074e3b14e906cf0817e4d
Sogaard464 <soren.sogaard.n@gmail.com>
2016-04-04 11:29:52 +0200

    Bruger login, første del
    indsat template til login, og lavet bruger klassen.

---
+++ GuideMeHome/app/src/main/java/gruppe3/dmab0914/guidemehome/User.java
@@ -0,0 +1,67 @@
+package gruppe3.dmab0914.guidemehome;
+
+/**
+ * Created by Seagaard on 04-04-2016.
+ */
+public class User {
+    private String mName;
+    private String mPhone;
+    private double mLongitude;
+    private double mLatitude;
+    private String mEmail;
+    private boolean mGender;
+
+    public User(String name, String phone, String email){
+        mName = name;
+        mPhone = phone;
+        mEmail = email;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public void setPhone(String phone) {
+        mPhone = phone;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(double longitude) {
+        mLongitude = longitude;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(double latitude) {
+        mLatitude = latitude;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        mEmail = email;
+    }
+
+    public boolean isGender() {
+        return mGender;
+    }
+
+    public void setGender(boolean gender) {
+        mGender = gender;
+    }
+}
